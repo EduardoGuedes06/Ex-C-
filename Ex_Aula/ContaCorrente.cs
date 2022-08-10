53e9aa9be74cb2c1caf5fe90b7282ed26228bf2c
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-10 15:36:31 -0300

    class

---
+++ Ex_Aula/ContaCorrente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Aula
+{
+    internal class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int num_agencia;
+        public string nome_agencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if(valor < 0){
+
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+
+
+
+
+    }
+}
---
+++ Ex_Aula/Program.cs
@@ -0,0 +1,31 @@
+﻿using Ex_Aula;
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Eduardo Guedes";
+conta1.conta = "10123ix";
+conta1.num_agencia = 23;
+conta1.nome_agencia = "Agencia Central";
+conta1.saldo = 1341.50;
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Amanda";
+conta2.conta = "92973ts";
+conta2.num_agencia = 28;
+conta2.nome_agencia = "Banco do Brasil";
+conta2.saldo = 100.50;
+
+
+Console.WriteLine("Titular: " + conta2.titular);
+Console.WriteLine("conta: " + conta2.conta);
+Console.WriteLine("numero de agencia: " + conta2.num_agencia);
+Console.WriteLine("nome da agencia " + conta2.nome_agencia);
+Console.WriteLine("Saldo " + conta2.saldo);
+
+Console.WriteLine("Saldo da Amanda pré-saque: "+conta2.saldo);
+conta2.Sacar(50);
+Console.WriteLine("Saldo da Amanda pós-saque: " + conta2.saldo);
+conta2.Depositar(50);
+Console.WriteLine("Saldo da Amanda pós-Deposito: " + conta2.saldo);
+Console.ReadKey();
+
+
